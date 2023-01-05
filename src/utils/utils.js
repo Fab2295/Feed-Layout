b41f6a40a06a1b0aaedf466f07f87b3822f7e8be
Dalcy Fabrício <fabricio221995@gmail.com>
2023-01-05 13:36:08 -0300

    lógica para adicionar datas ao projeto

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,18 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 export function getNextIdInArrayObject(array) {
     return (Math.max(...array.map(value => value.id)) + 1)
+}
+
+export function getPublishedDateFormattes(date) {
+    return format(date, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+    })
+}
+
+export function getTimeAgo(date = new Date().toDateString()) {
+    return formatDistanceToNow(date, {
+        addSuffix: true,
+        locale: ptBR
+    })
 }
